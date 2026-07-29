e41e78db73d503ab8e9b26f3f872cf2b14523255
JP-code33 <pateljhenil@gmail.com>
2026-07-29 12:48:48 -0400

    Completed

--- src/main.js
+++ src/main.js
@@ -14,9 +14,32 @@ fetch(`https://api.nasa.gov/planetary/apod?api_key=${API_KEY}`)
     }
 
     document.querySelector("#app").innerHTML = `
+    <div class="container">
       <h1>${data.title}</h1>
-      ${media}
-      <p>${data.explanation}</p>
+      
+      <div class="mainContent">
+        
+        <div class="media">
+          ${media}
+        </div>
+
+        <aside class="sideBar">
+          <div class="information">
+            <h2>Information</h2>
+            <p><strong>Date:</strong> ${data.date}</p>
+            <p><strong>Media:</strong> ${data.media_type}</p>
+            <p><strong>Copyright:</strong> ${data.copyright ?? "NASA"}</p>
+            <p><strong>Source:</strong> NASA APOD API</p>
+          </div>
+        </aside>
+        
+        <div class="explanation">
+            <h2>Explanation About Today's Picture</h2>
+            <p>${data.explanation}</p>
+        </div>
+
+      </div>
+        
     `;
   })
   .catch((err) => {
